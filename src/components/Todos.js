2297705f4ca740f0b2814771d9beed8948021968
Lova Wins <lovaunger@gmail.com>
2020-09-22 23:16:37 +0200

    todos

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,14 +4,18 @@ import PropTypes from 'prop-types';
 
 
 class Todos extends Component {
+ 
     render() {
         return this.props.todos.map((todo) => (
        <TodoItem  key={todo.id} todo={todo}
+       markComplete={this.markComplete}
         />
         ))
     }
 }
 
+
+// PropTypes
 Todos.propTypes = {
     todos: PropTypes.array.isRequired
 }
